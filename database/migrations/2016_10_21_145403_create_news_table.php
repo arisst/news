d39e1d930892f6c512d3728b2d038e3268df2e11
Aris Setyono <ariez.id@gmail.com>
2016-10-21 22:37:00 +0700

    create table news, categories

---
+++ database/migrations/2016_10_21_145403_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug', 255);
+            $table->string('title', 255);
+            $table->string('resume', 500);
+            $table->string('image', 500);
+            $table->text('content');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
